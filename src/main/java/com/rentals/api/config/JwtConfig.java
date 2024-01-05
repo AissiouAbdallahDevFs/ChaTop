2015e6cb58535f09c38018716dac403176d6e261
AissiouAbdallahDevFs <aissiou.abdallah@gmail.com>
2024-01-05 10:41:38 +0100

    Remove unused imports and whitespace in JwtConfig.java

--- src/main/java/com/rentals/api/config/JwtConfig.java
+++ src/main/java/com/rentals/api/config/JwtConfig.java
@@ -1,10 +1,7 @@
 package com.rentals.api.config;
 
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,6 +15,7 @@ public class JwtConfig {
 
 
     public String getJwtSecret() {
+
         return jwtSecret;
     }
   
